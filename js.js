428c4001326ed42f079a03e16f5adff2142f7577
ismailbgr <ozismailbugra@gmail.com>
2017-08-25 15:33:49 +0300

     

---
+++ js.js
@@ -0,0 +1,29 @@
+﻿window.onload=function(){
+	localStorage
+	if(localStorage.sifre == undefined){
+localStorage.setItem("sifre",prompt("Şifre Ne Olsun?"))
+document.getElementById("text").innerHTML = localStorage.yazi
+	}else{
+	var getsifre = prompt("Şifre ?")
+if(getsifre == localStorage.sifre){
+	document.getElementById("text").innerHTML = localStorage.yazi
+}else{
+	location.reload()
+}	
+}
+	
+	
+	
+	
+
+}
+
+function save(){
+	localStorage.setItem("yazi",document.getElementById("text").value)
+	alert("Kaydedildi")
+}
+
+function sifrekoy(){
+	localStorage.setItem("sifre",prompt("Şifre Ne Olsun?"))
+alert("Şifre Ayarlandı")
+	}
