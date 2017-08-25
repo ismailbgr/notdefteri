dae040d43c017f0cb0d6c8b2bded3fe1176bd10e
ismailbgr <ozismailbugra@gmail.com>
2017-08-25 18:54:15 +0300

     

--- js.js
+++ js.js
@@ -12,7 +12,26 @@ if(getsifre == localStorage.sifre){
 }	
 }
 	
-	
+	var cacheStatus = document.getElementById("cachestatus")
+	if (navigator.onLine) {
+        window.applicationCache.addEventListener('updateready', function(e) {
+            if (window.applicationCache.status == window.applicationCache.UPDATEREADY) {
+                window.applicationCache.swapCache();
+                cacheStatus.innerHTML="Güncel Bilgi Hazır!";
+                location.reload();
+
+
+            }
+        }, false);
+        window.applicationCache.addEventListener('cached', function(e) {cacheStatus.innerHTML="Önbellek Güncellenmeye Hazır"; },false);
+        window.applicationCache.addEventListener('noupdate', function(e) { cacheStatus.innerHTML="Önbellek Güncel!!"; },false);
+        window.applicationCache.addEventListener('downloading', function(e) { cacheStatus.innerHTML="İndiriiyor..."; },false);
+        window.applicationCache.addEventListener('error', function(e) { cacheStatus.innerHTML="Önbelleğe Alma Hatası"; },false);
+        window.applicationCache.update();
+    } else {
+        cacheStatus.innerHTML="Çevrimdışı";
+    }
+
 	
 	
 
